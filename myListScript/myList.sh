8841e60b982ddd597d3d69c9a1c08e48a90bf480
root <root@localhost.localdomain>
2015-09-02 18:31:25 +0000

    Opens html file in iceweasel

--- myListScript/myList.sh
+++ myListScript/myList.sh
@@ -1,8 +1,11 @@
 OUTPUTFILE="ls.html"
 touch $OUTPUTFILE
 chmod +rwx $OUTPUTFILE
-echo "<html>" >> $OUTPUTFILE
+echo "<html>" > $OUTPUTFILE
 echo "" >> $OUTPUTFILE
 ls -al >> $OUTPUTFILE
 echo "" >> $OUTPUTFILE
 echo "</html>" >> $OUTPUTFILE
+
+iceweasel -new-tab ./$OUTPUTFILE
+
