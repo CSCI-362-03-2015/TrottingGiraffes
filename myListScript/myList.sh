6ea4e0ced9804ec73ac84958f84c8db9343fcd25
root <root@localhost.localdomain>
2015-09-02 19:19:32 +0000

    Suppressed error output

--- myListScript/myList.sh
+++ myListScript/myList.sh
@@ -7,4 +7,4 @@ ls -al >> $OUTPUTFILE
 echo "" >> $OUTPUTFILE
 echo "</html>" >> $OUTPUTFILE
 
-iceweasel -new-tab ./$OUTPUTFILE || firefox -new-tab ./$OUTPUTFILE
+(iceweasel -new-tab ./$OUTPUTFILE || firefox -new-tab ./$OUTPUTFILE) 2>/dev/null
