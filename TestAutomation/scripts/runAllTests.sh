0310f53e5485455cd92c40e6908620ec9e4b1a0f
Steven Aldinger <aldingerst@g.cofc.edu>
2015-09-08 14:01:40 -0400

    Script parses testCase files now

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -7,12 +7,24 @@ TESTCASESEXEC="../testCasesExecutables"
 #Move to test cases directory
 #cd $TESTCASES // if we do this, we can't use variables for relative location
 
-ls $TESTCASES| while read line 
+ls $TESTCASES | while read FILENAME 
 do
-	echo "Run testcase executable based off of $line specifications."
+	echo "Run testcase executable based off of $FILENAME specifications."
 	echo ""
+
 	#test to see if we can cat all files in the testcases directory
-	cat "$TESTCASES"/"$line"
+	cat "$TESTCASES"/"$FILENAME" | while read TESTCASESLINE
+	do
+		#Get length of line string for formatting
+		TESTCASESLINELENGTH=${#TESTCASESLINE}
+		
+		#Cut off numbers at beginning of lines
+		TESTCASESLINECHOPPED="${TESTCASESLINE:3:$TESTCASESLINELENGTH}"
+		
+		#Delete comments in the future
+		echo ${TESTCASESLINECHOPPED%"#*"}
+	done
+ 
 	echo ""
 	echo""
 done
