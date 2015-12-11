d6390484f659ded8880a41780b839f401f757f78
Steven Aldinger <aldingerst@g.cofc.edu>
2015-12-10 19:51:51 -0500

    Added a waiting message

---
+++ TestAutomation/scripts/mightyGiraffes.sh
@@ -0,0 +1,56 @@
+for i in {0..10..1}
+  do
+     echo ""
+  done
+
+echo "
+
+                                                   M        
+                                                =  =M M     
+                                                ==    ZZ    
+                                               = =Z Z===    
+                                              =M Z M==ZM= M 
+                                                ZM      = ZZ
+                                             M              
+                                            ZMM M=          
+                                               MM           
+                                          =MM M             
+                                          M   M=            
+                                       Z M= MM              
+                                     Z M   =ZM              
+                                   M== MM =M=               
+                                M  M  MM M =                
+                              Z Z==M MM = =                 
+                             ZZ =M=M=M ZM                   
+                           Z   =  M  MM =                   
+                        M Z==ZM M MZM M=                    
+         =MMM      =ZZ = MZ=MZMMMM= M                       
+         ZM     ==M  Z M=MZM M MMM MMMM                     
+         M  MZMZ  M MMMZ= MMM=M=MMM ==M                     
+       ==ZM     =M MMMMZMZM  MM=M Z M M=                    
+        =          ==M M ZZ Z=MMZ MZZ  =                    
+            Z=Z MZZZZZZM  MMMZM MZMM =Z                     
+             = Z M Z   Z MM M MZ= ZM ZM                     
+           MM=Z =Z   M=     M===Z=   ==M                    
+             =  Z  Z=      =MM=      M                      
+         === =Z=M=Z        M M        MMZ                   
+            = =           Z M          =Z                   
+       =  Z                 M          =MZ                  
+       Z=ZZ                ==           =Z                  
+      M= =               Z                 Z                
+     Z=Z                  MM              Z Z               
+    M Z                  ZM                ZMM              
+   Z Z                    Z                 Z =             
+  =ZZ                                                       
+ ==                      =                    =             
+M                                              Z Z          
+                        =                         =         
+ZM                      Z                         Z=M       
+                      = =    =    ==   = ==== Z =           
+                                                    "
+for i in {0..2..1}
+  do
+     echo ""
+  done
+
+echo "Team Trotting Giraffes: Please wait..."
